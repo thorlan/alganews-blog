e224a96956921cdef8341a6ecf8bc6432d807559
thorlan <thiagorlandini@hotmail.com>
2021-12-04 13:51:36 -0300

    validando parametros do getServerSideProps

---
+++ components/Avatar.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import Image from 'next/image';
+import avatar from '../public/avatar.jpg';
+import { useState } from "react";
+
+interface AvatarProps{
+    src: string
+}
+
+export default function Avatar(props: AvatarProps){
+
+    const [src,setSrc] = useState(props.src);
+
+    return <Wrapper>
+        <StyledAvatar 
+            src={src}
+            width={40}
+            height={40}
+            onError={e => {
+                setSrc(avatar.src);
+            }}
+        />
+    </Wrapper>
+}
+
+const StyledAvatar = styled(Image)`
+    object-fit: cover;
+`
+
+const Wrapper = styled.div`
+    border-radius: 20px;
+    box-shadow: 0 0 0 4px ${p => p.theme.primaryForeground};
+    height: 40px;
+    width: 40px;
+    overflow: hidden;
+`;
--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -1,93 +1,124 @@
 import { Post } from "orlandini-sdk";
 import styled from "styled-components";
+import Avatar from "./Avatar";
+import Link from "next/link";
+import { transparentize } from "polished";
 
-interface FeaturedPosProps {
-    postSummary: Post.Summary
+interface FeaturedPostProps {
+  postSummary: Post.Summary;
 }
 
-export default function FeaturedPost(props: FeaturedPosProps) {
+export default function FeaturedPost(props: FeaturedPostProps) {
 
-    return <Wrapper>
-        <Tags>
-            {props.postSummary.tags.map(tag => <Tag key={tag}>{tag}</Tag>)}
-        </Tags>
-        <Editor>
+  const {id, slug} = props.postSummary;
+
+  return (
+    <Link href={`/posts/${id}/${slug}`} passHref>
+      <Wrapper>
+        <BgImage bg={props.postSummary.imageUrls.medium} />
+        <Content>
+          <Tags>
+            {props.postSummary.tags.map((tag) => (
+              <Tag key={tag}>{tag}</Tag>
+            ))}
+          </Tags>
+          <Editor>
             <Avatar src={props.postSummary.editor.avatarUrls.small} />
             <EditorDescription>
-                <EditorName>
-                    {props.postSummary.editor.name}
-                </EditorName>
-                <PostDate>  
-                   há 3 dias
-                </PostDate>
+              <EditorName>{props.postSummary.editor.name}</EditorName>
+              <PostDate>ha 3 dias</PostDate>
             </EditorDescription>
-        </Editor>
-        <Title>{props.postSummary.title}</Title>
-
-    </Wrapper>
+          </Editor>
+          <Title>{props.postSummary.title}</Title>
+        </Content>
+      </Wrapper>
+    </Link>
+    );
 }
 
-const Wrapper = styled.div`
-    background-color: ${p => p.theme.primaryBackground};
-    color:${p => p.theme.primaryForeground};
-    border-radius: ${p => p.theme.borderRadius};
+const Content = styled.div`
+  position: relative;
+  gap: 24px;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-    gap: 24px;
+const BgImage = styled.div<{ bg: string }>`
+  background-image: url(${(p) => p.bg});
+  position: absolute;
+  inset: 0;
+  background-color: blue;
+  z-index: 0;
+  opacity: 0.05;
+`;
 
-    padding: 32px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+const Wrapper = styled.a`
+  position: relative;
+  background-color: ${(p) => p.theme.primaryBackground};
+  color: ${(p) => p.theme.primaryForeground};
+  border-radius: ${(p) => p.theme.borderRadius};
+  overflow: hidden;
+  padding: 32px;
+  width: 100%;
+  min-height: 256px;
 
-    width: 100%;
-    min-height: 256px;
+  text-decoration: none;
+
+  display: flex;
+  align-items: center;
+
+  transition: box-shadow 0.25s ease;
+
+  &:hover,
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 4px ${p => transparentize(0.7,p.theme.primaryBackground)};
+  }
 `;
 
 const Tags = styled.ul`
-    list-style: none;
-    display: flex;
-    gap: 8px;
+  list-style: none;
+  display: flex;
+  gap: 8px;
 `;
 
 const Tag = styled.li`
-    background-color: ${p => p.theme.activeElementBackground};
-    color: ${p => p.theme.activeElementForeground};
-    border-radius: ${p => p.theme.borderRadius};
-    text-transform: lowercase;
-    padding: 4px 12px;
-    cursor: default;
+  background-color: ${(p) => p.theme.activeElementBackground};
+  color: ${(p) => p.theme.activeElementForeground};
+  border-radius: ${(p) => p.theme.borderRadius};
+  text-transform: lowercase;
+  padding: 4px 8px;
+  cursor: default;
+  font-size: 12px;
+
+  @media screen and (max-width: 767px){
+    display: none;
+  }
 `;
 
 const Editor = styled.div`
-    display: flex;
-    gap: 16px;
+  display: flex;
+  gap: 16px;
 `;
 
 const EditorDescription = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 `;
 
 const PostDate = styled.p`
-    font-size: 12px;
+  font-size: 12px;
 `;
 
 const EditorName = styled.p`
-    font-size: 14px;
-    font-weight: 700;
+  font-size: 14px;
+  font-weight: 700;
 `;
 
-const Avatar = styled.img`
-    height: 40px;
-    width: 40px;
-    object-fit: cover;
-    border-radius: 20px;
-    box-shadow: 0 0 0 4px ${p => p.theme.primaryForeground};
-`
-
 const Title = styled.h2`
-    font-size: 24px;
-    font-weight: 700;
+  font-size: 24px;
+  font-weight: 700;
 `;
